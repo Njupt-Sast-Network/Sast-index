75a938755aba099bcf7a7e24e328ad50f1b5ca46
sdygt <sdygt@users.noreply.github.com>
2016-07-14 02:20:57 +0800

    optimize gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,71 @@
-var gulp = require('gulp'),
-    ugjs = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    clean = require('gulp-clean'),
-    replace = require('gulp-replace'),
-    rev = require('gulp-rev-append'),
-    ifElse = require('gulp-if-else'),
-    htmlreplace = require('gulp-html-replace'),
-    browserSync = require('browser-sync'),
-    base64 = require('gulp-base64'),
-    php = require('gulp-connect-php');
-var postcss = require('gulp-postcss'); //postcss本身
-var autoprefixer = require('autoprefixer');
-var precss = require('precss'); //提供像scss一样的语法
-var cssnano = require('cssnano'); //更好用的css压缩!
-var sourcemaps = require('gulp-sourcemaps');
-var path = './src/',
-    csspath = './src/css/**/*.css',
-    jspath = './src/js/**/*.js',
-    htmlpath = './src/views/**/*.html',
-    ifonpath = './src/fonts/**';
-var disPath = 'production/Public/',
-    disCssPath = 'production/public/css',
-    disJsPath = 'production/Public/js',
-    disHtmlPath = 'production/Application/Index/View',
-    disifonpath = 'production/Public/fonts';
+const gulp         = require('gulp'),
+      uglify       = require('gulp-uglify'),
+      imagemin     = require('gulp-imagemin'),
+      del          = require('del'),
+      replace      = require('gulp-replace'),
+      rev          = require('gulp-rev-append'),
+      ifElse       = require('gulp-if-else'),
+      htmlreplace  = require('gulp-html-replace'),
+      base64       = require('gulp-base64'),
+      postcss      = require('gulp-postcss'),
+      autoprefixer = require('autoprefixer'),
+      cssnano      = require('cssnano');
+
+const SRC              = './src/',
+      SRC_CSS          = SRC + 'css/**/*.css',
+      SRC_JS           = SRC + 'js/**/*.js',
+      SRC_FONTS        = SRC + 'fonts/**',
+      SRC_VIEWS        = SRC + 'views/**/*.html',
+      SRC_STATIC_HTML  = SRC + 'html/*.html',
+      SRC_IMG          = SRC + 'images/**/*.*',
+
+      DIST             = './production/Public/',
+      DIST_CSS         = DIST + 'css/',
+      DIST_JS          = DIST + 'js/',
+      DIST_FONTS       = DIST + 'fonts/',
+      DIST_VIEWS       = 'production/Application/Index/View/',
+      DIST_STATIC_HTML = DIST + 'html/',
+      DIST_IMG         = DIST + 'images/';
+
 var urlTag = '';
 var NODE_ENV = '';
-const reload = browserSync.reload;
 
-
-gulp.task('ugjs', function() {
-    return gulp.src(jspath).pipe(replace('__target__', urlTag))
+gulp.task('uglify', function () {
+    return gulp.src(SRC_JS).pipe(replace('__target__', urlTag))
                .pipe(replace('../../', '../../Public/'))
-               .pipe(ifElse(NODE_ENV === 'public', ugjs))
-               .pipe(gulp.dest(disJsPath));
+               .pipe(ifElse(NODE_ENV === 'public', uglify))
+               .pipe(gulp.dest(DIST_JS));
 });
 
-gulp.task('css', function() {
+gulp.task('css', function () {
     var processes = [cssnano];
-    gulp.src(csspath)
-        .pipe(ifElse(NODE_ENV === 'public', function() {
+    gulp.src(SRC_CSS)
+        .pipe(ifElse(NODE_ENV === 'public', function () {
             return postcss(processes)
         }))
         .pipe(base64({
             extensions: ['png', /\.jpg#datauri$/i],
             maxImageSize: 10 * 1024 // bytes,
         }))
-        .pipe(gulp.dest(disCssPath));
+        .pipe(gulp.dest(DIST_CSS));
 });
 
 
-gulp.task('images', function() {
-    return gulp.src('src/images/**/*.*')
+gulp.task('images', function () {
+    return gulp.src(SRC_IMG)
                .pipe(imagemin({
                    progressive: true
                }))
-               .pipe(gulp.dest('production/Public/images'));
+               .pipe(gulp.dest(DIST_IMG));
 });
 
-gulp.task('iconfont', function() {
-    return gulp.src(ifonpath)
-               .pipe(gulp.dest(disifonpath));
+gulp.task('copy_fonts', function () {
+    return gulp.src(SRC_FONTS)
+               .pipe(gulp.dest(DIST_FONTS));
 });
 
-gulp.task('view', ['clean'], function() {
-    return gulp.src(htmlpath)
+gulp.task('build_view', function () {
+    return gulp.src(SRC_VIEWS)
                .pipe(rev())
                .pipe(replace('__target__', urlTag))
                .pipe(replace('..\/..\/', '__PUBLIC__/'))
@@ -75,41 +76,27 @@ gulp.task('view', ['clean'], function() {
                        tpl: ''
                    }
                }))
-               .pipe(gulp.dest(disHtmlPath));
+               .pipe(gulp.dest(DIST_VIEWS));
 });
 
-gulp.task('clean', function() {
-    return gulp.src(disHtmlPath, {
-        read: true
-    })
-               .pipe(clean());
+gulp.task('clean', function () {
+    return del([DIST, DIST_VIEWS])
 });
 
-gulp.task('build', function() {
+gulp.task('copy_static_html', function () {
+    return gulp.src(SRC_STATIC_HTML)
+               .pipe(gulp.dest(DIST_STATIC_HTML));
+});
+
+gulp.task('build', ['clean'], function () {
     NODE_ENV = 'public';
-    gulp.start('view', 'ugjs', 'css', 'iconfont', 'images');
+    gulp.start('build_view', 'uglify', 'css', 'copy_fonts', 'images', 'copy_static_html');
 });
 
-gulp.task('auto', function() {
-    gulp.watch([csspath], ['css']);
-    gulp.watch([jspath], ['ugjs']);
-    gulp.watch([htmlpath], ['view']);
-    gulp.watch('./src/component/*.html', ['component']);
+gulp.task('auto', function () {
+    gulp.watch([SRC_CSS], ['css']);
+    gulp.watch([SRC_JS], ['uglify']);
+    gulp.watch([SRC_VIEWS], ['build_view']);
+    gulp.watch([SRC_STATIC_HTML], ['copy_static_html']);
+    gulp.watch([SRC_FONTS], ['copy_fonts']);
 });
-// gulp.task('reload', function() {
-//     browserSync.init(chtmlpath, {
-//         startPath: "/views/",
-//         server: cpath,
-//         notify: false
-//     });
-//     gulp.watch([csasspath]).on('change', function() {
-//         runSequence('collegesass', 'collegecss', function() {
-//             bsReload();
-//         });
-//     });
-//     gulp.watch([cjspath], ['collegejs']).on('change', bsReload);
-//     gulp.watch([chtmlpath], ['collegeview']).on('change', bsReload);
-// });
-// gulp.task('php',function () {
-//     php.server({ base: 'production', port: 8010, keepalive: true});
-// })
