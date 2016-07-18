80c81af435fe75b93c267fca2032b5d672e10e9c
Tianyue Cui <Tianyue Cui>
2016-07-18 13:11:47 +0800

    静态修改部分完成

--- Application/Index/Controller/IndexController.class.php
+++ Application/Index/Controller/IndexController.class.php
@@ -4,11 +4,15 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index(){
     	$date = $this -> news();
+    	// F('news',NULL);
     	$this -> assign('news',F('news'));
     	$this -> display();
     }
     private function news(){
-    	$news = D('News') -> field('title,text') -> where() -> select();
+    	$news = D('News') -> field('title,text') -> where()->order('news_id desc') -> select();
     	if (!F('news')) F('news',$news);
+    }
+    public function mobile(){
+        $this ->display();
     }
 }
