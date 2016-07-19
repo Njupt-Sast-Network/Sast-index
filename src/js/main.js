63256d48cff497a66d9739ccb169165c120727e1
sdygt <sdygt@users.noreply.github.com>
2016-07-19 23:22:30 +0800

    import gulp
    usage:
    npm install
    gulp build

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+(function() {
+	//滚动事件
+	$(document).scroll(function() {
+		if($("html body").scrollTop() > 800) {
+			$("section.sectionOne .row3 .col-md-3 span").animate({opacity:"1"},600,function() {
+				$(this).next().animate({opacity:"1"},300)
+			});
+		}
+		if($("html body").scrollTop() > 1300){
+			$("section.sectionOne .row5").animate({opacity:"1"},300);
+		}
+		if($("html body").scrollTop() > 10) {
+			$("#hui").css("display","inline-block");
+		}else {
+			$("#hui").css("display","none");
+		}
+		if($("html body").scrollTop() > $("section.sectionOne .row6").offset().top-400 ) {
+			for(var i = 0; i < 10; i++) {
+				$("section.sectionOne .row6 .intro"+i).addClass("ani"+i);
+			}
+			
+		}
+	});
+	$("#hui").click(function() {
+		$("html body").animate({"scrollTop":"0"});
+	});
+	$(".down").click(function() {
+		$("html body").animate({"scrollTop":$("section.sectionOne").offset().top});
+	});
+})();
