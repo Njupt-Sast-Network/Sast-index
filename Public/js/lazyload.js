9491c14a64567ca267e5b435eb534b4a3ca04be4
airbob,gu <guliujian@users.noreply.github.com>
2016-08-12 17:41:03 +0800

    Update lazyload.js

--- Public/js/lazyload.js
+++ Public/js/lazyload.js
@@ -5,8 +5,6 @@
 		photoObj = $(".Box img");
 		setTimeout(function() {
 			for(var i = 0;i < 20;i++) {
-				
-				console.log(i)
 				photoObj.eq(i).attr("src","/Public/images/img"+i+".jpg");
 			}
 			},1000);
